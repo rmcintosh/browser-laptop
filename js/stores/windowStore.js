f87607ce20bad0768558392ee8c239b97271f3ed
bridiver <github@brianjohnson.cc>
2017-05-30 16:45:57 -0700

    don’t use active frame auditors @bbondy

--- js/stores/windowStore.js
+++ js/stores/windowStore.js
@@ -319,7 +319,7 @@ const doAction = (action) => {
         // For about:newtab we want to have the urlbar focused, not the new frame.
         // Otherwise we want to focus the new tab when it is a new frame in the foreground.
         if (action.location !== getTargetAboutUrl('about:newtab')) {
-          focusWebview(frameStateUtil.activeFrameStatePath(windowState))
+          focusWebview(statePath)
         }
         break
       }
